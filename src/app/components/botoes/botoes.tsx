f674376241cff2bbbb1c5d0c43d009c56165e29d
aronisouza <aronisouza@yahoo.com.br>
2023-07-01 16:36:14 -0300

    principal

--- src/app/components/botoes/botoes.tsx
+++ src/app/components/botoes/botoes.tsx
@@ -1,22 +1,22 @@
 import "./botoes.scss"
 export function Botoes() {
 return(
-    <div className="botoes">
-          <h2>Sobre</h2>
-          <p>
-          Resumo:
-          Analista desenvolvedor programador de C# com habilidades em desenvolvimento de aplicações web e desktop utilizando .NET Core e .NET Framework . Possuo conhecimento em banco de dados SQL Server.
-          Experiência em trabalhar em equipe para alcançar objetivos de negócios.
-          Conhecimento no processo de análise de projeto, levantamento de requisitos, modelagem de dados e caso de testes.
-          Conhecimento em Design Patterns e metodologias de desenvolvimento de software.
-          Atuação com SGBD's SQL Server, MySQL.
-          Forte conhecimento de orientação a objetos.
-          Desenvolvimento de código semântico.
-          Sites adequados para dispositivos móveis.
-          Otimização de desempenho e SEO.
-          Estudando Reactj e node.
-          </p>
-        <button>Entrar em contato</button>
-      </div>
-    )
+  <div className="botoes">
+        <h2>Sobre</h2>
+        <p>
+        Resumo:
+        Analista desenvolvedor programador de C# com habilidades em desenvolvimento de aplicações web e desktop utilizando .NET Core e .NET Framework . Possuo conhecimento em banco de dados SQL Server.
+        Experiência em trabalhar em equipe para alcançar objetivos de negócios.
+        Conhecimento no processo de análise de projeto, levantamento de requisitos, modelagem de dados e caso de testes.
+        Conhecimento em Design Patterns e metodologias de desenvolvimento de software.
+        Atuação com SGBD's SQL Server, MySQL.
+        Forte conhecimento de orientação a objetos.
+        Desenvolvimento de código semântico.
+        Sites adequados para dispositivos móveis.
+        Otimização de desempenho e SEO.
+        Estudando Reactj e node.
+        </p>
+      <button>Entrar em contato</button>
+    </div>
+  )
 }
