0041516bae1b8b19a1eefd43f29edd6c4ae28209
aronisouza <aronisouza@yahoo.com.br>
2023-07-09 09:41:24 -0300

    Modificação do header

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,16 +1,41 @@
+import Image from "next/image"
 import "./header.scss"
 export function Header() {
   return(
     <div className="header">
-      <div>
-        <h2>Olá 👋 sou Aroni Souza</h2>
-        <h1>Desenvolvedor</h1>
-        <h3>C#,PHP, NodeJs Reactjs</h3>
-        🌎 🐧 📃
+      <div className="headerinfo">
+
+        <div>
+          <h1>Desenvolvedor</h1>
+          <h3>C#, PHP, NodeJs Reactjs</h3>
+          <h2>Olá 👋 sou Aroni Souza</h2>
+        </div>
+        
+        <div className="divIcons">
+          <Image
+            src="/www.png"
+            alt="www"
+            width={30}
+            height={30}
+            />
+          <Image
+            src="/github.png"
+            alt="www"
+            width={30}
+            height={30}
+          />
+          <Image
+            src="/linkedin.png"
+            alt="www"
+            width={30}
+            height={30}
+          />
+        </div>
+      
+      </div>
         <p>
           TENHO 41 ANOS SOU DESENVOLVEDOR BACKEND E FRONTEND COM 2 ANOS DE EXPERIENCIA TRABALHANDO COMO FREELANCER
         </p>
-      </div>
     </div>
   )
 }
